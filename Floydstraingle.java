5d7ccf6894502d6a858d0b9694c785e69770f0fd
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-11 10:03:13 +0530

    Floyds traingle

---
+++ Floydstraingle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Floydstraingle {
+    public static void traingle(int n) {
+        int counter = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        traingle(n);
+
+
+    }
+}
