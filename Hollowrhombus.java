5e2a92faf44590c6424dfd35c217492e1db6bea8
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-12 10:52:21 +0530

    Prints Hollow Rhombus

---
+++ Hollowrhombus.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+// Prints Hollow rhombus Pattern 
+public class hollowrhombus {
+    public static void rhombus(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n; j++) {
+                if(i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        rhombus(n);
+    }
+}
+
