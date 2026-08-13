8a450356a8b6de067160980141c11c5dab03153e
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-13 23:11:35 +0530

    Prints Number Pattern

---
+++ NumberPattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class NumberPattern {
+    public static void numberPyramid(int n) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2*i-1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Number:");
+    int n = sc.nextInt();
+    numberPyramid(n);
+    }
+}
