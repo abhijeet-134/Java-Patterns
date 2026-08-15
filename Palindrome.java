20d5a59b34960a2de16fd4eaccd5b38a01de7a81
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-15 12:23:38 +0530

    Prints Pyramid palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,22 +1,28 @@
 import java.util.*;
 
-
-public class Pattern {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+// Prints Plaindrome Numbers
+public class Palindrome {
+    public static void Palindrome(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
                 System.out.print("  ");
             }
 
-            for (int j = i; j >= 1; j--) {
+
+            for(int j=i; j>=1; j--) {
                 System.out.print(j + " ");
             }
-
-            for (int j = 2; j <= i; j++) {
+            for(int j=2; j<=i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
+    }
+     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        Palindrome(n);
+        
     }
 }
